2bad943afd3bd5f6238cf31d15a26a130300a22d
ruru <142723369+ruru-m07@users.noreply.github.com>
2023-10-20 17:20:32 +0530

    Add files via upload

--- db.js
+++ db.js
@@ -1,19 +1,11 @@
 const mongoose = require("mongoose");
-const mongoURI = "mongodb://127.0.0.1:27017/study_community";
-const Chatroom = require("./models/Chatroom");
+require("dotenv").config();
+const mongoURI = process.env.MONGO_URI;
+
 
 const connectToMongo = async () => {
-  // var watchMongodb = function () {
-  //   const chatroom = Chatroom
-  //   console.log(chatroom)
-  //   const changeStream = chatroom.watch();
-  //   changeStream.on("change", (next) => {
-  //     // process next document
-  //   });
-  // };
   mongoose.connect(mongoURI, () => {
     console.log("Connected to Mongo Successfully");
-    // watchMongodb();
   });
 };
 
