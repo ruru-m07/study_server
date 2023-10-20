0c60d6db2d9e7bdc7e8d14ecd0ebe2e42c02bc4c
ruru <142723369+ruru-m07@users.noreply.github.com>
2023-10-20 17:59:46 +0530

    Add files via upload

--- db.js
+++ db.js
@@ -2,12 +2,13 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const mongoURI = process.env.MONGO_URI;
 
-
 const connectToMongo = async () => {
-  mongoose.connect(mongoURI, () => {
-    console.log("Connected to Mongo Successfully");
+  await mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   });
-};
 
+  console.log("Connected to MongoDB");
+};
 
 module.exports = connectToMongo;
--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const connectToMongo = require("./db");
 var cors = require("cors");
 const http = require("http");
 const express = require("express");
-const path = require("path");
-const { Server } = require("socket.io");
-const auth = require("./routes/auth");
 const ChatRoute = require("./routes/ChatRoute");
 const MessageRoute = require("./routes/MessageRoute");
 const multer = require("multer");
@@ -12,25 +9,10 @@ const multer = require("multer");
 connectToMongo();
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["POST", "GET"],
-  },
-});
-
-let activeUsers = [];
-
 const port = 3005;
 
 app.use(cors());
 app.use(express.json());
-
-// Routes
-// app.use(express.static(path.resolve("./public")));
-// app.get("/", (req, res) => {
-//   res.sendFile("/public/index.html");
-// });
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/posts", require("./routes/post"));
 app.use("/chat", ChatRoute);
